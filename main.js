3ecf31dff830678c5e02e7afa394f9951ca15587
Miranda Morris <juniperistic@gmail.com>
2024-03-27 21:38:03 -0400

    Homepage & Blogpage Creation

--- main.js
+++ main.js
@@ -6,39 +6,14 @@ hamburgerMenu.addEventListener('click', () => {
     sideMenu.classList.toggle('active');
 });
 
-sideMenuItems.forEach((link) => {
+sideMenuItems.foreach((link) => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
         sideMenuItems.forEach((item) => {
+            item.classList.add('active');
         });
         link.classList.add('active');
     });
 });
 
 
-
-// var tablinks = document.getElementsByClassName("tab-links");
-// var tabcontents = document.getElementsByClassName("tab-contents");
-
-// function opentab(tabname){
-//     for(tablink of tablinks){
-//         tablink.classList.remove("active-link");
-//     }
-
-//     for(tabcontent of tabcontents){
-//         tabcontent.classList.remove("active-tab");
-//     }
-
-//     event.currentTarget.classList.add("active-link");
-//     document.getElementById(tabname).classList.add("active-tab");
-// }
-
-// var sidemenu = document.getElementById("sidemenu");
-
-// function openmenu(){
-//     sidemenu.style.right = "0";
-// }
-
-// function closemenu(){
-//     sidemenu.style.right = "-200px";
-// }
